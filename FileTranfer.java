0b1f848e3b7140b67db0c35fc75edb3fac761bcd
Remil <51848204+RemilRLs@users.noreply.github.com>
2025-02-11 23:12:39 +0100

    Add files via upload

---
+++ FileTranfer.java
@@ -0,0 +1,11 @@
+class FileTransfer {
+    String sender;
+    String filename;
+    String filePath;
+
+    public FileTransfer(String sender, String filename, String filePath) {
+        this.sender = sender;
+        this.filename = filename;
+        this.filePath = filePath;
+    }
+}
